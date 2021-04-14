b135b7d49b9c8904519ff9e1ae89dfd5a39c24c1
MrakCw | Denis <mrakcw@gmail.com>
2021-04-14 23:14:55 +0300

    push
    Signed-off-by: MrakCw | Denis <mrakcw@gmail.com>

---
+++ js/getintouch.js
@@ -0,0 +1,34 @@
+const wrapper = document.createElement("div");
+
+wrapper.innerHTML = `
+<link rel="stylesheet" href="https://mrakcw.github.io/cdn/css/getintouch.css">
+<div class="contact-wrapper" id="mrakcw">
+    <div class="contact-panel">
+        <div class="contact-panel__header">
+          <a href="https://nick-name.ru/nickname/mrakcw/" target="_blank" style="color: #eee; font-family: 'Pacifico', cursive;font-size: 21px;">MrakCw Design</a>
+        </div>
+        <button class="contact-close"><i class="fa fa-times"></i></button>
+        <ul>
+          <li><a href="https://t.me/mrakcw" target="_blank" title="Telegram"><i class="fa fa-telegram"></i></a></li>
+          <li><a href="https://vk.com/mrakcw" target="_blank" title="VK"><i class="fa fa-vk"></i></a></li>
+          <li><a href="https://fb.com/mrakcw" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/mrakcw" target="_blank" title="LinkedIn"><i class="fa fa-linkedin"></i></a></li>
+          <li><a href="https://github.com/mrakcw" target="_blank" title="GitHub"><i class="fa fa-github"></i></a></li>
+          <li><a href="https://steamcommunity.com/id/mrakcw/" target="_blank" title="Steam"><i class="fa fa-steam"></i></a></li>
+        </ul>
+    </div>
+</div>
+<button class="contact-button" style="font-family: 'Pacifico', cursive;">MrakCw Design</button>`;
+
+document.body.appendChild(wrapper);
+const contactButton = document.querySelector(".contact-button");
+const contactClose = document.querySelector(".contact-close");
+const contactWrapper = document.querySelector(".contact-wrapper");
+
+contactButton.addEventListener("click", () => {
+  contactWrapper.classList.toggle("contact-wrapper--open");
+});
+
+contactClose.addEventListener("click", () => {
+  contactWrapper.classList.remove("contact-wrapper--open");
+});
